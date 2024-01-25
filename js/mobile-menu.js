1b24aeae1da68836308156eb35ba84eac1028142
nayavu <145128666+nayavua@users.noreply.github.com>
2024-01-25 20:29:00 +0200

    site-finally

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,8 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuBtnOpen = document.querySelector('.menu-btn-open');
-const menuBtnClose = document.querySelector('.menu-btn-close');
+const menuBtnClose = document.querySelectorAll('.menu-btn-close');
 
 const toggleMenu = () => mobileMenu.classList.toggle('is-open');
 
 menuBtnOpen.addEventListener('click', toggleMenu);
-menuBtnClose.addEventListener('click', toggleMenu);
+menuBtnClose.forEach(btn => btn.addEventListener('click', toggleMenu));
